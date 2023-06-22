75e00234770ce4474b4a81db28f2113aa2dd12cc
Birger Schacht <birger@rantanplan.org>
2023-06-22 13:23:25 +0200

    feat(deploy-apis-instance): add apis startup commands to start.sh

--- deploy-apis-instance/start.sh
+++ deploy-apis-instance/start.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+red() {
+	echo -e "\e[0;31m Running> $@ \e[0m"
+	$@
+	echo -e "\e[0;31m Done> $@ \e[0m"
+}
+red django-admin migrate
+red django-admin collectstatic
+[[ $CREATE_RELATIONSHIPS == "True" ]] && red django-admin create_relationships
 gunicorn wsgi -b 0.0.0.0:5000 --timeout 120 --workers=3 --threads=3 --worker-connections=1000
